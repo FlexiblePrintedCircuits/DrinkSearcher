59cdcea6f271bd65d39f5d0ad60f87a8e6a4eef1
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-27 11:25:49 +0900

    new commit

--- main.py
+++ main.py
@@ -72,43 +72,68 @@ def handle_message(event):
     if event.type == "message":
         DrinkTorF = DrinkSearch(event.message.text)
         if (event.message.text == "学生課前"):
+            Image = ImageSendMessage(
+            original_content_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/GAKUSE-KA.jpg?raw=true",
+            preview_image_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/GAKUSE-KA.jpg?raw=true"
+            )
             line_bot_api.reply_message(
                 event.reply_token,
                 [
                     TextSendMessage(text="お茶、サイダーなどがあります。"),
+                    Image,
                     TextSendMessage(text="https://www.google.com/maps?q=34.482481,136.825055")
                 ]
             )
         if (event.message.text == "潮騒会館"):
+            Image = ImageSendMessage(
+            original_content_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/SHIOSAI.jpg?raw=true",
+            preview_image_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/SHIOSAI.jpg?raw=true"
+            )
             line_bot_api.reply_message(
                 event.reply_token,
                 [
                     TextSendMessage(text="コーヒー、カフェオレなどがあります。"),
+                    Image,
                     TextSendMessage(text="https://www.google.com/maps?q=34.482616,136.824430")
 
                 ]
             )
         if (event.message.text == "図書館下"):
+            Image = ImageSendMessage(
+            original_content_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/TOSHOKAN2.jpg?raw=true",
+            preview_image_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/TOSHOKAN2.jpg?raw=true"
+            )
             line_bot_api.reply_message(
                 event.reply_token,
                 [
                     TextSendMessage(text="コーラ、ファンタなどがあります。"),
+                    Image,
                     TextSendMessage(text="https://www.google.com/maps?q=34.482325,136.824341")
                 ]
             )
         if (event.message.text == "寮食堂前"):
+            Image = ImageSendMessage(
+            original_content_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/RYOSHOKU.jpg?raw=true",
+            preview_image_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/RYOSHOKU.jpg?raw=true"
+            )
             line_bot_api.reply_message(
                 event.reply_token,
                 [
                     TextSendMessage(text="コーラ、ファンタなどがあります。"),
+                    Image,
                     TextSendMessage(text="https://www.google.com/maps?q=34.480867,136.825025")
                 ]
             )
         if (event.message.text == "B棟"):
+            Image = ImageSendMessage(
+            original_content_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/B-to.jpg?raw=true",
+            preview_image_url="https://github.com/FlexiblePrintedCircuits/DrinkSearcherPic/blob/master/B-to.jpg?raw=true"
+            )
             line_bot_api.reply_message(
                 event.reply_token,
                 [
                     TextSendMessage(text="カルピスソーダ、モンスターなどがあります。"),
+                    Image,
                     TextSendMessage(text="https://www.google.com/maps?q=34.480462,136.824733")
                 ]
             )
@@ -131,61 +156,3 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
-
-'''import os
-
-from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, MessageAction, TemplateSendMessage,
-    ButtonsTemplate)
-
-app = Flask(__name__)
-app.debug = False
-
-line_bot_api = LineBotApi("54SfB4WOh1G2/yf/1j3+BQdIGOAElTuieI0y12hqJ04+BsK3i5AVwXcD5TBYmp8hQzEKT9qC/lic8q4cdrG3KdIJKXJhr7QR+i+gxjkYkpHB4px4h4duTaMlR8iz2Vu57gKKGel9CUq1OVvBsO+r5QdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("84f1dc304d0714dfa5266f0c10a99b00")
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def response_message(event):
-    profile = line_bot_api.get_profile(event.source.user_id)
-
-    status_msg = profile.status_message
-    if status_msg != "None":
-        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
-        status_msg = "なし"
-
-    messages = TemplateSendMessage(alt_text="Buttons template",
-                                   template=ButtonsTemplate(
-                                       thumbnail_image_url=profile.picture_url,
-                                       title=profile.display_name,
-                                       text=f"User Id: {profile.user_id}\n",
-                                       actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
-
-    line_bot_api.reply_message(event.reply_token, messages=messages)
-
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)'''
